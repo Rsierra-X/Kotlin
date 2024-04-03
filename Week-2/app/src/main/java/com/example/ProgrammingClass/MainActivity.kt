8664b227be0cc2593020f62ff88852393cc1ac74
Raul Sierra <jsierra@labymed.com.gt>
2024-04-02 23:09:37 -0600

    feat: setting validator for the radio buttons and the values empties

--- Week-2/app/src/main/java/com/example/ProgrammingClass/MainActivity.kt
+++ Week-2/app/src/main/java/com/example/ProgrammingClass/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.ProgrammingClass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.core.view.get
 import com.example.ProgrammingClass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val btAction = binding.button
+        val radio = binding.radioGroup
+        val value1 = binding.val1.text.toString()
+        val value2 = binding.val2.text.toString()
+        val value3 = binding.val3.text.toString()
+
+        var radioButton: String = ""
+
+        radio.setOnCheckedChangeListener { _, checkedId ->
+            val radioSelected: RadioButton = findViewById(checkedId)
+            radioButton = radioSelected.text.toString()
+        }
+
+        btAction.setOnClickListener {
+            if (radioButton == ""){
+                Toast.makeText(this, "No se tiene ninguna opcion seleccionada", Toast.LENGTH_LONG).show()
+            }else if (value1 == "" || value2 == "" || value3 == ""){
+                Toast.makeText(this, "Se tienen que llenar todos los campos de valores", Toast.LENGTH_LONG).show()
+            }else {
+                Toast.makeText(this, "Se tiene un valor", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
